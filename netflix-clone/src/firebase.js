75d58926ee738a906919af523d4715c273108cbb
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-23 11:20:04 +0200

    adding firebase config

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIB8vLoiAaVcqxNUrfW3rlOKqvobSJLHM",
+    authDomain: "netflix-clone-fc38c.firebaseapp.com",
+    projectId: "netflix-clone-fc38c",
+    storageBucket: "netflix-clone-fc38c.appspot.com",
+    messagingSenderId: "164405180560",
+    appId: "1:164405180560:web:49b849627b4e164af88015"
+  };
