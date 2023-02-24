f63992b32979132c649114298f003ec8b08a4139
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-24 10:02:37 +0200

    navbar appearance after scrolling

--- netflix-clone/src/components/Nav.js
+++ netflix-clone/src/components/Nav.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/styles/Nav.css'
 import logo from '../assets/images/netflix-logo.png'
 import avatar from '../assets/images/avatar-logo.jpg'
 
 function Nav() {
-  return (
-    <div className='nav nav__black'>
-        <div className="nav__container">
-            <img className='nav__logo' src={logo} alt="logo"/>
-            <img className='nav__icon' src={avatar} alt="avatar"/>
+    const [show, handelShow] = useState(false);
+
+    const transitionNavbar = () => {
+        if(window.scrollY > 100){
+            handelShow(true)
+        }else{
+            handelShow(false)
+        };  
+    };
+
+    useEffect(() =>{
+        window.addEventListener('scroll', transitionNavbar);
+        return () => window.removeEventListener('scroll', transitionNavbar);
+    },[]);
+
+
+
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <div className="nav__container">
+                <img className='nav__logo' src={logo} alt="logo" />
+                <img className='nav__icon' src={avatar} alt="avatar" />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Nav
+export default Nav;
--- netflix-clone/src/layouts/HomeScreen.js
+++ netflix-clone/src/layouts/HomeScreen.js
@@ -6,6 +6,63 @@ function HomeScreen() {
   return (
     <div className='homeScreen'>
         <Nav/>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+
     </div>
   )
 }
