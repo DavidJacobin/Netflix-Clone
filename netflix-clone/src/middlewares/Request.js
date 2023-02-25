a8e4b84c5648e2da8fcadc086a0869ce74748fdd
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-25 12:30:06 +0200

    fetching movies from api

--- netflix-clone/src/middlewares/Request.js
+++ netflix-clone/src/middlewares/Request.js
@@ -1,8 +1,8 @@
 
-const API_KEY = 'ksfjajdsf'
+const API_KEY = 'f81980ff410e46f422d64ddf3a56dddd'
 
 const requests = {
-
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`
 };
 
 
