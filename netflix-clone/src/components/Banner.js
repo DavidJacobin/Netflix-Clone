4210c2af4acbfda1ff21e27ecea41c16e752b76b
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-26 15:29:15 +0200

    setting up the image path

--- netflix-clone/src/components/Banner.js
+++ netflix-clone/src/components/Banner.js
@@ -31,7 +31,7 @@ function Banner() {
     <header className='banner' style={
         {
             backgroundSize: 'cover',
-            backgroundImage: `url('https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg')`,
+            backgroundImage: `url('https://image.tmbd.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: 'center center',
             }}
         >
