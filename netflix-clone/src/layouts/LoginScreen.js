51b2deb53680b6c5075e5d45a77ef2c864c1e85e
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-27 10:12:03 +0200

    setting up the css for the login page

--- netflix-clone/src/layouts/LoginScreen.js
+++ netflix-clone/src/layouts/LoginScreen.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import '../assets/styles/LoginScreen.css'
+import logo from '../assets/images/netflix-logo.png'
+import back from '../assets/images/back.jpg'
 
 function LoginScreen() {
   return (
     <div className='loginScreen'>
-        <div className='background'>
-            <img src=''></img>
+        <img className='background__img' alt="" src={back} />
 
-        </div>
+      <div className="background">
+        <img className='loginScreen__logo' src={logo} alt="logo" />
+        <button className='loginScreen__button'>Sing in</button>
+        <div className='loginScreen__gradient'/>
+      </div>
     </div>
   )
 }
