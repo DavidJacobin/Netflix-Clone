174d39fffb1fb813ec1eca0e54bd85478e4c7e72
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-27 08:38:18 +0200

    setting up login screen

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
 import './App.css';
 import HomeScreen from './layouts/HomeScreen';
+import LoginScreen from './layouts/LoginScreen.js';
 
 function App() {
+  
   return (
     <div className="app">
 
+
       <Router>
         <Routes>
-          <Route path='/' element={<HomeScreen />}>
-    
-          </Route>
+        <Route path='/' element={<HomeScreen />}/>
+        <Route path='/login' element={<LoginScreen />}/>
+
         </Routes>
+        
       </Router>
     </div>
   );
---
+++ netflix-clone/src/layouts/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/styles/LoginScreen.css'
+
+function LoginScreen() {
+  return (
+    <div className='loginScreen'>
+        <h1>HELOO
+
+        </h1>
+    </div>
+  )
+}
+
+export default LoginScreen
